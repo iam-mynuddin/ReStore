3cd2d793a38674cc05e635a5b05861c26f6a04b2
iam-mynuddin <mynuddin.shaik234@gmail.com>
2024-05-05 06:29:09 +0530

    ReactBasics

---
+++ API/Controllers/ProductsController.cs
@@ -0,0 +1,22 @@
+﻿using API.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    public class ProductsController : MyBaseApiController
+    {
+        private readonly StoreContext _context;
+
+        public ProductsController(StoreContext context)
+        {
+            _context = context;
+        }
+        [HttpGet("getproducts")]
+        public IActionResult GetAllProducts()
+        {
+            var resObjList= _context.Products.ToList();
+            return Ok(resObjList);
+        }
+    }
+}
